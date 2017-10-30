218280c8f8c2f941290e9cdd240908c687906bc7
alex <alexvstoia@gmail.com>
2017-10-30 00:40:44 +0000

    Add new grails test helper plugins

--- jaxrs-jersey1/grails-app/conf/logback.groovy
+++ jaxrs-jersey1/grails-app/conf/logback.groovy
@@ -20,4 +20,5 @@ if (Environment.isDevelopmentMode() && targetDir) {
         }
     }
     logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
+    logger("org.grails.datastore.gorm", DEBUG, ['FULL_STACKTRACE'], false)
 }
